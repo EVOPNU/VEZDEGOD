784072335a688c2d22a0eb55823b4b9b46111d06
LAPTOP-QR73DSMV\agent <merkulovwork@mail.ru>
2022-06-18 13:53:10 +1000

    init frontend

---
+++ wheel-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Registration from './components/logUp/logUp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Registration />
+);
